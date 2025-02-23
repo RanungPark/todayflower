adf3edfba6fa07e0cca521be1843add864ee968b
woong <ckehfqkr302@naver.com>
2025-02-23 20:32:58 +0900

    feat: input 컴포넌트 input 속성 도록 추가

--- src/@types/common.ts
+++ src/@types/common.ts
@@ -1,2 +1,3 @@
 export type Size = 'sm' | 'md' | 'lg';
 export type Variant = 'primary' | 'secondary' | 'tertiary';
+export type ValidationState = 'default' | 'error' | 'success';
---
+++ src/components/inputField/Input.tsx
@@ -0,0 +1,47 @@
+import React, { forwardRef } from 'react';
+import styled from 'styled-components';
+
+import { ValidationState } from 'src/@types/common';
+
+import { TextFieldStyles } from './styles';
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  validationState?: ValidationState;
+  testId?: string;
+}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ validationState = 'default', testId, ...props }, ref) => {
+    return (
+      <InputStyled
+        $validationState={validationState}
+        data-cy={testId}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+
+Input.displayName = 'Input';
+
+const InputStyled = styled.input<{ $validationState: ValidationState }>`
+  ${({ theme }) => theme.typography.Caption};
+  ${({ $validationState }) =>
+    TextFieldStyles[$validationState] || TextFieldStyles.default};
+  display: block;
+
+  width: 100%;
+  height: 56px;
+  padding: 16px;
+
+  background-color: ${({ theme }) => theme.colors.white};
+
+  cursor: text;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray};
+  }
+`;
+
+export default Input;
