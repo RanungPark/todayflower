30dcbe3204c3f2f0fcd2541064c57543c4c26a81
woong <ckehfqkr302@naver.com>
2025-02-23 20:33:35 +0900

    move: 폴더 이동

---
+++ src/components/inputField/components/HelpMessage.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { ReactComponent as CheckCircle } from '@assets/icons/wght300/CheckCircle.svg';
+import { ReactComponent as CrossCircle } from '@assets/icons/wght300/CrossCircle.svg';
+import { mixins } from '@styles/Mixin';
+import { ValidationState } from 'src/@types/common';
+
+import { HelpMessageStyles } from '../styles';
+
+interface HelpMessageProps {
+  children: React.ReactNode;
+  validationState: ValidationState;
+  testId?: string;
+}
+
+const HelpMessage = ({
+  children,
+  validationState = 'default',
+  testId,
+}: HelpMessageProps) => {
+  return (
+    <HelpMessageWrapper $validationState={validationState}>
+      {validationState === 'error' && <CrossCircle aria-label="cross circle" />}
+      {validationState === 'success' && (
+        <CheckCircle aria-label="check circle" />
+      )}
+      <p data-cy={testId}>{children}</p>
+    </HelpMessageWrapper>
+  );
+};
+
+const HelpMessageWrapper = styled.div<{ $validationState: ValidationState }>`
+  ${mixins.flexBox({})};
+  ${({ theme }) => theme.typography.CaptionSmall};
+  ${({ $validationState }) =>
+    HelpMessageStyles[$validationState] || HelpMessageStyles.default};
+  position: absolute;
+  bottom: -20px;
+  height: 16px;
+
+  & svg {
+    width: 16px;
+    height: 16px;
+  }
+`;
+
+export default HelpMessage;
---
+++ src/components/inputField/components/Label.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
+  children: React.ReactNode;
+}
+
+const Label = ({ children, ...props }: LabelProps) => {
+  return <LabelStyled {...props}>{children}</LabelStyled>;
+};
+
+const LabelStyled = styled.label`
+  ${({ theme }) => theme.typography.Heading6}
+  position: absolute;
+  top: -30px;
+  width: 100%;
+`;
+
+export default Label;
