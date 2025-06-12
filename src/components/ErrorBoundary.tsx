24b9ba7afce5e3b57f7d9946545373fd6c15bc04
_wo_.ong <ckehfqkr302@gmail.com>
2025-06-13 02:12:35 +0900

    feat: 에러 바운더리 작성하여 에러처리 중앙화 할수 있도록 작성

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,53 @@
+import {Component, type ReactNode} from 'react';
+
+interface RenderFallbackProps<ErrorType extends Error = Error> {
+  error: ErrorType;
+  reset: () => void;
+}
+
+interface Props {
+  children: ReactNode;
+  fallback: (props: RenderFallbackProps) => ReactNode;
+  onReset?: () => void;
+}
+
+interface State {
+  error: Error | null;
+}
+
+const initialState: State = {
+  error: null,
+};
+
+class ErrorBoundary extends Component<Props, State> {
+  state: State = initialState;
+
+  static getDerivedStateFromError(error: Error): State {
+    return {error};
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    // eslint-disable-next-line no-console
+    console.error('Uncaught error:', error, errorInfo);
+  }
+
+  reset = () => {
+    this.props.onReset?.();
+    this.setState(initialState);
+  };
+
+  render() {
+    const {error} = this.state;
+
+    if (error !== null) {
+      return this.props.fallback({
+        error,
+        reset: this.reset,
+      });
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
